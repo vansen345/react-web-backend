7624de722b11fe601189c60c44f4b8fda6f2b228
vansen345 <86304461+vansen345@users.noreply.github.com>
2026-04-24 21:06:30 +0700

    -- change status

--- src/controllers/authen_controller.ts
+++ src/controllers/authen_controller.ts
@@ -7,25 +7,25 @@ export const registerUser = async (req: Request, res: Response) => {
     try {
         const { email } = req.body;
         if (!email) {
-            return res.status(400).json({ success: false, message: 'Email is required' });
+            return res.status(400).json({ status: "error", message: 'Email is required' });
         }
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
-            return res.status(400).json({ success: false, message: 'Invalid email format' });
+            return res.status(400).json({ status: "error", message: 'Invalid email format' });
         }
         const user = await UserModel.findOne({ email })
         if (user) {
             return res.status(400).json({
-                success: false,
+                status: "error",
                 message: 'Email already exists'
             })
         }
         await UserModel.create({ email })
         const otp = genOtp(email)
         await sendMail(email, 'Mã OTP đăng ký', `<p>Mã OTP của bạn là: <strong>${otp}</strong></p><p>Mã có hiệu lực trong 5 phút.</p>`)
-        res.status(200).json({ success: true, message: 'OTP sent successfully',elements:email });
+        res.status(200).json({ status: "true", message: 'OTP sent successfully',elements:email });
     } catch (error) {
-        res.status(500).json({ success: false, message: 'Send OTP failed', elements: error });
+        res.status(500).json({ status: "error", message: 'Send OTP failed', elements: error });
 
     }
 }
